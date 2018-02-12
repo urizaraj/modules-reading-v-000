56ad0763517c5c907e4c104ed5d6babed5760992
Alexander Urizar <urizaraj@gmail.com>
2018-02-12 23:14:53 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,6 +1,9 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
+require("fancy_dance.rb")
+
+
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
